e3a314275caec58c5e5e8106f73af8bd8002f0e3
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-05 18:28:29 +0200

    Query 6 completed

--- sample.sql
+++ sample.sql
@@ -22,3 +22,8 @@ WHERE `period` = "I %" AND `year` = 1;
 SELECT *
 FROM `exams`
 WHERE `date` = "2020-06-20" AND `hour` > "14:00:00";
+
+-- QUERY 6
+SELECT *
+FROM `degrees`
+WHERE `level` = "magistrale";
